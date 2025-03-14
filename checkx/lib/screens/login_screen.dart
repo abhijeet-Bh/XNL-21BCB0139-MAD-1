12317383c033a00d858b9f9226f89bd037bf0beb
abhijeet-Bh <abhijeetbhardwaj53@gmail.com>
2025-03-14 12:18:25 +0530

    fix: design fix

--- checkx/lib/screens/login_screen.dart
+++ checkx/lib/screens/login_screen.dart
@@ -21,54 +21,71 @@ class LoginScreen extends StatelessWidget {
         ),
         child: Padding(
           padding: AppTheme.pagePadding,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              SvgPicture.asset(
-                Constants.checkXLogo,
-                width: 142,
-              ),
-              const SizedBox(height: 20),
-              Text(
-                "Login to continue",
-                style: AppTheme.displayText.copyWith(fontSize: 32),
-              ),
-              const SizedBox(height: 10),
-              Text(
-                "Login using Google or Apple to continue your CheckX Account",
-                textAlign: TextAlign.left,
-                style: AppTheme.bodyTextLarge.copyWith(color: AppTheme.accent),
-              ),
-              const SizedBox(height: 40),
-              Center(
-                child: SvgPicture.asset(
-                  Constants.currencyIcon,
-                  height: 258,
-                  width: 258,
+          child: SafeArea(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const SizedBox(height: 40),
+                    SvgPicture.asset(
+                      Constants.checkXLogo,
+                      width: 233,
+                    ),
+                    const SizedBox(height: 20),
+                    Text(
+                      "Login to continue",
+                      style: AppTheme.displayText.copyWith(
+                        fontSize: 32,
+                        fontWeight: FontWeight.normal,
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    Text(
+                      "Login using Google or Apple to continue your CheckX Account",
+                      textAlign: TextAlign.left,
+                      style: AppTheme.bodyTextLarge
+                          .copyWith(color: AppTheme.accent),
+                    ),
+                  ],
                 ),
-              ),
-              const SizedBox(height: 50),
-              PrimaryButton(
-                text: "Continue with Google",
-                onPressed: () {
-                  // TODO: Implement Google OAuth
-                },
-                color: AppTheme.accent,
-                icon: Constants.googleIcon,
-                textColor: Colors.black,
-              ),
-              const SizedBox(height: 15),
-              PrimaryButton(
-                text: "Continue with Apple",
-                onPressed: () {
-                  // TODO: Implement Apple OAuth
-                },
-                color: AppTheme.primary,
-                icon: Constants.appleIcon,
-                textColor: Colors.black,
-              ),
-            ],
+                Center(
+                  child: SvgPicture.asset(
+                    Constants.currencyIcon,
+                    height: 258,
+                    width: 258,
+                  ),
+                ),
+                const SizedBox(height: 50),
+                Column(
+                  children: [
+                    PrimaryButton(
+                      text: "Continue with Google",
+                      onPressed: () {
+                        // TODO: Implement Google OAuth
+                      },
+                      color: AppTheme.accent,
+                      icon: Constants.googleIcon,
+                      textColor: Colors.black,
+                    ),
+                    const SizedBox(height: 22),
+                    PrimaryButton(
+                      text: "Continue with Apple",
+                      onPressed: () {
+                        // TODO: Implement Apple OAuth
+                      },
+                      color: AppTheme.primary,
+                      icon: Constants.appleIcon,
+                      textColor: Colors.black,
+                    ),
+                    const SizedBox(height: 22),
+                  ],
+                )
+              ],
+            ),
           ),
         ),
       ),
